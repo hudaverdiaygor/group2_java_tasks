313a2e7acb67ac195a0bded5aa99e58a35ddfc26
hudaverdiaygor <aygorhudaverdi@gmail.com>
2025-12-30 11:56:47 -0800

    Project is created.

---
+++ src/java_interview_tasks/hudaverdi/task1.java
@@ -0,0 +1,11 @@
+package java_interview_tasks.hudaverdi;
+
+public class task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("First test is completed!");
+
+    }
+}
